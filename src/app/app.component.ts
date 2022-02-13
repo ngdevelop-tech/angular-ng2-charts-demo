9f02b7e9d21b984c2350ba0c2020c6b6349df440
ngdevelop-tech <aprajapati06@gmail.com>
2022-02-13 12:32:03 +0530

    ng2-charts line and bar chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { ChartData, ChartOptions } from 'chart.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'angular-ng2-charts-demo';
+  salesData: ChartData<'line'> = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
+    datasets: [
+      { label: 'Mobiles', data: [1000, 1200, 1050, 2000, 500], tension: 0.5 },
+      { label: 'Laptop', data: [200, 100, 400, 50, 90], tension: 0.5 },
+      { label: 'AC', data: [500, 400, 350, 450, 650], tension: 0.5 },
+      { label: 'Headset', data: [1200, 1500, 1020, 1600, 900], tension: 0.5 },
+    ],
+  };
+
+  chartOptions: ChartOptions = {
+    responsive: true,
+    plugins: {
+      title: {
+        display: true,
+        text: 'Monthly Sales Data',
+      },
+    },
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
